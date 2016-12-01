a7422ba9602f56da30f19b17ef7cb2ec2209c083
Jason <jcflinn14@gmail.com>
2016-11-30 23:23:27 -0600

    edit app.js with genres and a few books to work with

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,49 @@
-'use strict';
+(function () {
+    'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    angular.module('readingList', []);
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    var genres = ['folklore', 'fantasy', 'fiction',
+                  'non-fiction', 'thriller', 'romance',
+                  'horror', 'humor', 'legend',
+                  'mystery', 'mythology', 'poetry']
+    var books = [
+        {
+            title: 'The Last Wish: Introducing The Witcher',
+            author: 'Andrzej Sapkowski',
+            isbn: '0316029181',
+            review: '"Like a complicated magic spell, a Sapkowski novel is a hodgepodge of fantasy, intellectual discourse, and dry humor. Recommended."―Time',
+            rating: 4,
+            genres: {
+                'fantasy': true,
+                'fiction': true,
+                'folklore': true
+            }
+        },
+        {
+            title: 'Mageborn: The Blacksmith\'s Son',
+            author: 'Michael G. Manning',
+            isbn: '1463684347',
+            review: '"You\'ve GOT to read this!"―Mom',
+            rating: 4,
+            genres: {
+                'fantasy': true,
+                'fiction': true
+            }
+        },
+        {
+            title: 'The Alchemyst: The Secretes of the Immortal Nicholas Flamel',
+            author: 'Michael Scott',
+            isbn: '0385736002',
+            review: '"Entertaining and engaging for all ages!"―E. Walker',
+            rating: 5,
+            genres: {
+                'fantasy': true,
+                'fiction': true
+            }
+        }
+    ];
+});
+
+
